15fc76234912a22d9dfa84af51f83ddd9dea085c
soumyanayak95 <soumyanayak95@gmail.com>
2023-12-13 21:04:21 +0530

    first commit

---
+++ src/test/java/com/srn/designPatterns/DesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srn.designPatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
